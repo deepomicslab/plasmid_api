d763fa7ce91c1a8b81f107af3a2d1721b57d2c57
Eric Feng <xikanfeng2@gmail.com>
2024-06-28 03:45:25 +0800

    fix bug

--- database/utils.py
+++ database/utils.py
@@ -6,7 +6,9 @@ def root_path():
 
 def esm_fold_api(sequence, pdb_file):
     api_url = 'https://api.esmatlas.com/foldSequence/v1/pdb/'
-    x = requests.post(api_url, data = sequence[:390], verify=False)
+    sequence = sequence[:390]
+    print(len(sequence))
+    x = requests.post(api_url, data = sequence, verify=False)
     with open(pdb_file, 'w') as output:
         print(x.text)
         output.write(x.text)
