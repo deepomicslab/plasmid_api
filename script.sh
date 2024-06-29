6dd63d268a93b144adbacd012571d3e82d496c0f
Eric Feng <xikanfeng2@gmail.com>
2024-06-29 08:06:40 +0800

    fix bug

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ module load GCCcore/11.2.0 GCC/11.2.0 Python/3.9.6
 
 while true
 do
-    /apps/software/Python/3.9.6-GCCcore-11.2.0/bin/python /home/platform/phage_db/phage_api/manage.py crontab run 6da2da0ecaf88b312e144325c8c7f4ba
+    python manage.py crontab run 6da2da0ecaf88b312e144325c8c7f4ba
     echo run contab
     sleep 1m
 done
