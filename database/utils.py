3986c0e07533dcd6159507bcca39617787a5146d
Eric Feng <xikanfeng2@gmail.com>
2024-09-11 01:56:30 +0800

    fix bug

--- database/utils.py
+++ database/utils.py
@@ -5,7 +5,7 @@ def root_path():
     return os.path.dirname(os.path.abspath(__file__))
 
 def esm_fold_api(sequence, pdb_file=None):
-    api_url = 'https://api.esmatlas.com/foldSequence/v1/pdb/'
+    api_url = 'https://api.esmatlas.com/foldSequence/v1/cif/'
     x = requests.post(api_url, data = sequence[:399], verify=False)
     if pdb_file:
         with open(pdb_file, 'w') as output:
